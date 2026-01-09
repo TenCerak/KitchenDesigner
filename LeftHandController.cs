cc9ed7e76eb3aba42a086c5a1deae5f31ba04558
TenCerak <cerovsky2@gmail.com>
2026-01-09 19:04:31 +0100

    přilepení menu k ruce a zobrazení po zmačnuktí tlačítka

---
+++ LeftHandController.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+using System.Threading.Tasks.Dataflow;
+
+public partial class LeftHandController : XRController3D
+{
+	[Export]
+	Node3D handMenu;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.ButtonPressed += buttonPress;
+        ButtonReleased += buttonRelease;
+    }
+
+    private void buttonPress(string name)
+    {
+        GD.Print("Pressed: " + name);
+        switch (name)
+        {
+            case "grip_click":
+                handMenu.Visible = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void buttonRelease(string name)
+    {
+        GD.Print("Released: " + name);
+        switch (name)
+        {
+
+            case "grip_click":
+                handMenu.Visible = false;
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{
+	}
+}
