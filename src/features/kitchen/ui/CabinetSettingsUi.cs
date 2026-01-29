0f9e24c18694fb3019a29a9af3e11026c699afd6
TenCerak <cerovsky2@gmail.com>
2026-01-29 11:27:05 +0100

    vylepšení UI nastavení skříňky o zobrazení aktuálních hodnot

--- src/features/kitchen/ui/CabinetSettingsUi.cs
+++ src/features/kitchen/ui/CabinetSettingsUi.cs
@@ -7,8 +7,13 @@ namespace KitchenDesigner.Features.Kitchen.UI
     public partial class CabinetSettingsUi : Control, IMenuPage
     {
         [Export] public HSlider WidthSlider;
+        [Export] public Label WidthValueLabel;
+
         [Export] public VSlider HeightSlider;
+        [Export] public Label HeightValueLabel;
+
         [Export] public VSlider DepthSlider;
+        [Export] public Label DepthValueLabel;
 
         [Export] public SpinBox ShelfCount;
 
@@ -25,6 +30,10 @@ namespace KitchenDesigner.Features.Kitchen.UI
                 WidthSlider.Value = _data.Width;
                 WidthSlider.ValueChanged -= OnWidthChanged;
                 WidthSlider.ValueChanged += OnWidthChanged;
+                if(WidthValueLabel != null)
+                {
+                    WidthValueLabel.Text = $"{_data.Width * 100:N0} cm";
+                }
             }
 
             if (HeightSlider != null)
@@ -32,12 +41,20 @@ namespace KitchenDesigner.Features.Kitchen.UI
                 HeightSlider.Value = _data.Height;
                 HeightSlider.ValueChanged -= OnHeightChanged;
                 HeightSlider.ValueChanged += OnHeightChanged;
+                if(HeightValueLabel != null)
+                {
+                    HeightValueLabel.Text = $"{_data.Height * 100:N0} cm";
+                }
             }
             if (DepthSlider != null)
             {
                 DepthSlider.Value = _data.Depth;
                 DepthSlider.ValueChanged -= OnDepthChanged;
                 DepthSlider.ValueChanged += OnDepthChanged;
+                if(DepthValueLabel != null)
+                {
+                    DepthValueLabel.Text = $"{_data.Depth * 100:N0} cm";
+                }
             }
 
             if (ShelfCount != null)
@@ -51,15 +68,24 @@ namespace KitchenDesigner.Features.Kitchen.UI
         private void OnWidthChanged(double value)
         {
             if (_data != null) _data.Width = (float)value;
+
+            if (WidthValueLabel is null) return;
+            WidthValueLabel.Text = $"{value * 100:N0} cm";
         }
 
         private void OnHeightChanged(double value)
         {
             if (_data != null) _data.Height = (float)value;
+
+            if (HeightValueLabel is null) return;
+            HeightValueLabel.Text = $"{value * 100:N0} cm";
         }
         void OnDepthChanged(double value)
         {
             if (_data != null) _data.Depth = (float)value;
+
+            if (DepthValueLabel is null) return;
+            DepthValueLabel.Text = $"{value * 100:N0} cm";
         }
 
         private void OnShelfChanged(double value)
